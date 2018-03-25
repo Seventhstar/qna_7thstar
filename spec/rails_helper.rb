e35a4cf9b330d7daaf9c53a128449e9fa161c8e7
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2018-03-25 11:23:31 +0300

    rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
 #  config.include ActionController, type: :feature
